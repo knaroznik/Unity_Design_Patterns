de243801e631c0169a89223aaa5a5028a36a8038
knaroznik <Redivivatus@wp.pl>
2018-04-30 21:24:28 +0200

    Object Pool.

---
+++ Design Patterns/Assets/Scripts/ObjectPool/ObjectPool.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ObjectPool{
+	private Dictionary<ObjType, List<GameObject>> _available = new Dictionary<ObjType, List<GameObject>>();
+	private Dictionary<ObjType, GameObject> _prototypes = new Dictionary<ObjType, GameObject> ();
+
+	private static ObjectPool instance = null;
+	public static ObjectPool GetInstance()
+	{
+		if (instance == null)
+		{
+			instance = new ObjectPool();
+		}
+		return instance;
+	}
+	public GameObject acquireReusable(ObjType type)
+	{
+		List<GameObject> reusables = _available [type];
+		if (reusables.Count > 0)
+		{
+			GameObject item = reusables[0];
+			reusables.RemoveAt(0);
+			item.SetActive (true);
+			return item;
+		}
+		else
+		{
+			GameObject obj = GameObject.Instantiate (_prototypes [type]);
+			Debug.Log ("CREATING " + type.ToString ());
+			return obj;
+		}
+	}
+
+	public void ReleaseReusable(GameObject item, ObjType type)
+	{
+		_available [type].Add (item);
+		item.SetActive (false);
+	}
+
+	public void AddPrototype(GameObject obj, ObjType type){
+		_prototypes.Add (type, obj);
+	}
+
+	public void InitPool(){
+		for (int i = 0; i < _prototypes.Count; i++) {
+			_available.Add (_prototypes.Keys.ToArray()[i], new List<GameObject> ());
+		}
+
+	}
+}
